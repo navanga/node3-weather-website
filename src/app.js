ff5bbdb3dd41c2928433875473dec1fc09bcb728
navangaw <nwanigatunga@whiteclarkegroup.com>
2020-05-01 15:53:45 +1000

    Add about test

--- src/app.js
+++ src/app.js
@@ -49,7 +49,7 @@ app.get('/help', (req, res) => {
 app.get('/weather', (req, res) => {
     if(!req.query.address) {
         return res.send({
-            error: 'You must provide aaddress'
+            error: 'You must provide address'
         })
     }
 
